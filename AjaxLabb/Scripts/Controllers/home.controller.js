ed009fb57ff6c61b8db9c8ba4225048ca77af575
John Fristedt <john.fristedt@chas.se>
2016-10-05 09:59:15 +0200

    Labb finished

---
+++ AjaxLabb/Scripts/Controllers/home.controller.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("mainModule")
+    .controller("HomeController", [
+        "$scope",
+        "postsApi",
+        function ($scope, postsApi) {
+            $scope.title = "Home";
+            $scope.newPost = {};
+
+            console.log($scope.subscribedAuthors);
+
+            $scope.subscribe = function (author) {
+                $scope.subscribedAuthors.push(author);
+            };
+
+            $scope.addPost = function () {
+                postsApi.addPost($scope.newPost)
+                .then(function (data) {
+                    console.log(data);
+                    $scope.posts.push(data);
+                });
+            };
+            
+            $scope.deletePost = function (index) {
+                postsApi.deletePost($scope.posts[index].id)
+                    .then(function () {
+                        $scope.posts.splice(index, 1);
+                    })
+            };
+        }
+    ]);
---
+++ AjaxLabb/Scripts/Controllers/main.controller.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("mainModule")
+    .controller("MainController", [
+        "$scope",
+        "$location",
+        "$route",
+        "postsApi",
+        function ($scope, $location, $route, postsApi) {
+            $scope.$route = $route;
+            $scope.posts = [];
+            $scope.subscribedAuthors = [];
+
+            postsApi.getPosts()
+                .then(function (data) {
+                    $scope.posts = data;
+                })
+
+            $scope.go = function (url) {
+                $location.path(url);
+            };
+        }
+    ]);
